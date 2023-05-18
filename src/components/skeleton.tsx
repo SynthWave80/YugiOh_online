bb8412dcd2e5e25d9611589a8895bb4bb2af0fb8
SynthWave80 <63927952+SynthWave80@users.noreply.github.com>
2023-05-18 23:45:29 +0100

    Added Search bar

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -5,24 +5,22 @@ import { Placeholder, Spinner } from "react-bootstrap";
 
 const Skeleton = () => {
   return (
-    <div className="d-flex justify-content-around">
-      <Card style={{ width: "18rem" }}>
-        <Spinner className="mx-auto p-4" />
-        <Card.Body>
-          <Placeholder as={Card.Title} animation="glow">
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{" "}
-            <Placeholder xs={6} /> <Placeholder xs={8} />
-          </Placeholder>
-          <div className="flex items-center justify-between">
-            <Placeholder.Button variant="primary" xs={3} />
-            <Placeholder.Button variant="primary" xs={3} />
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card className=" w-52">
+      <Spinner className="mx-auto p-4" />
+      <Card.Body>
+        <Placeholder as={Card.Title} animation="glow">
+          <Placeholder xs={6} />
+        </Placeholder>
+        <Placeholder as={Card.Text} animation="glow">
+          <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{" "}
+          <Placeholder xs={6} /> <Placeholder xs={8} />
+        </Placeholder>
+        <div className="flex items-center justify-between">
+          <Placeholder.Button variant="primary" xs={3} />
+          <Placeholder.Button variant="primary" xs={3} />
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,27 @@ import PaginatedItems from "@/components/pagination";
 import { Spinner } from "react-bootstrap";
 import Skeleton from "@/components/skeleton";
 import { Card, Placeholder } from "react-bootstrap";
+import { string } from "zod";
+import { useState, useEffect } from "react";
+import { Alert } from "react-bootstrap";
 
 const Home: NextPage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searching, setSearching] = useState(false);
+
   const { data, error, refetch, isLoading } = api.cards.getAll.useQuery();
+
+  const {
+    data: searchData,
+    isLoading: searchIsLoading,
+    refetch: searchRefetch,
+  } = api.cards.getCardsLike.useQuery(searchTerm);
+
+  useEffect(() => {
+    console.log(`Search content: ${searchTerm}`);
+    console.log(searchData);
+  }, [searchTerm]);
+
   const items = 12;
 
   return (
@@ -22,24 +40,45 @@ const Home: NextPage = () => {
 
       <div className="flex h-20 items-center border-b border-gray-300 bg-gradient-to-b from-cyan-500 to-blue-500">
         <div className="text-5xl text-white">YuGiOh db.</div>
+
+        <div className="relative flex items-center pl-20 text-gray-600">
+          <input
+            className="h-10 w-72 rounded-lg border-2 border-gray-300 bg-white px-5 pr-16 text-sm focus:outline-none"
+            type="search"
+            name="search"
+            placeholder="Search"
+            onChange={(text) =>
+              text.target.value.length > 1
+                ? (setSearchTerm(text.target.value), setSearching(true))
+                : setSearching(false)
+            }
+          />
+          {!searching && (
+            <Alert variant="info">This is a alert—check it out!</Alert>
+          )}
+        </div>
       </div>
 
-      <div className="flex min-h-full min-w-full items-center justify-center bg-gradient-to-b from-blue-500 to-sky-950 p-4 ">
-        {isLoading && (
-          <div className=" grid grid-cols-4  gap-4 pb-5">
+      <div className="flex min-h-full min-w-full  bg-gradient-to-b from-blue-500 to-sky-950 p-4 ">
+        {isLoading || searchIsLoading ? (
+          <div className="grid grid-cols-4 gap-3">
             {Array.from({ length: items }).map((_, index) => (
               <Skeleton key={index} />
             ))}
           </div>
-        )}
+        ) : null}
 
-        {data && (
-          <PaginatedItems
-            itemsPerPage={items}
-            cards={data}
-            isLoading={isLoading}
-          />
-        )}
+        <PaginatedItems
+          itemsPerPage={items}
+          cards={
+            data && !searching
+              ? data
+              : searchData && searching
+              ? searchData
+              : undefined
+          }
+          isLoading={isLoading}
+        />
       </div>
     </>
   );
